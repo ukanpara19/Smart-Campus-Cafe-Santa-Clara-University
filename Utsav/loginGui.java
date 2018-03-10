ab40dabc6b9c832573bc5e12dc3050c2ed194928
ukanpara <33382057+ukanpara@users.noreply.github.com>
2018-03-10 14:51:37 -0800

    Update loginGui.java

--- Utsav/loginGui.java
+++ Utsav/loginGui.java
@@ -67,33 +67,49 @@ public class loginGui extends JFrame {
 		
 		setTitle("Smart Campus Cafe -  Login");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 311);
+		setBounds(100, 100, 633, 634);
 		JLabel Title = new JLabel("Smart Campus Cafe");
-		Title.setForeground(Color.BLACK);
-		Title.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 20));
+		Title.setBounds(171, 81, 278, 30);
+		Title.setForeground(new Color(0, 51, 102));
+		Title.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 24));
 //		 frame = new JFrame();
 		 
-		setContentPane(new JLabel(new ImageIcon("/Users/utsavkanpara/eclipse-workspace/SmartCampuscafe/src/SmartCampusCafe/blue.jpg")));
+	
 		
 		JLabel lblSantaClaraUniversity = new JLabel("Santa Clara University");
-		lblSantaClaraUniversity.setForeground(Color.BLACK);
-		lblSantaClaraUniversity.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 22));
+		lblSantaClaraUniversity.setBounds(161, 28, 302, 30);
+		lblSantaClaraUniversity.setForeground(new Color(0, 51, 102));
+		lblSantaClaraUniversity.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 24));
 		
 		JLabel lblLoginDetails = new JLabel("Login Details :");
-		lblLoginDetails.setFont(new Font("Arial", Font.BOLD, 14));
+		lblLoginDetails.setBounds(33, 139, 169, 30);
+		lblLoginDetails.setForeground(new Color(0, 51, 102));
+		lblLoginDetails.setBackground(new Color(255, 255, 255));
+		lblLoginDetails.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 22));
 		
 		JLabel lblUsername = new JLabel("User ID :");
-		lblUsername.setFont(new Font("Arial", Font.BOLD, 16));
+		lblUsername.setBounds(49, 196, 85, 28);
+		lblUsername.setForeground(new Color(0, 51, 102));
+		lblUsername.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 18));
 		
 		JLabel lblPassword = new JLabel("Password :");
-		lblPassword.setFont(new Font("Arial", Font.BOLD, 16));
+		lblPassword.setBounds(49, 266, 99, 34);
+		lblPassword.setBackground(SystemColor.menu);
+		lblPassword.setForeground(new Color(0, 51, 102));
+		lblPassword.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 18));
 		
 		Username_textField = new JTextField();
+		Username_textField.setBounds(178, 188, 285, 36);
+		Username_textField.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 15));
+		Username_textField.setBackground(new Color(255, 255, 255));
+		Username_textField.setForeground(new Color(0, 0, 0));
 		Username_textField.setColumns(15);
 		
 		password_field = new JPasswordField(15);
+		password_field.setBounds(178, 279, 285, 36);
 		
 		JButton btn_login = new JButton("Login");
+		btn_login.setBounds(245, 352, 116, 42);
 		btn_login.addActionListener(new ActionListener() {
 			
 			@SuppressWarnings("deprecation")
@@ -105,7 +121,7 @@ public class loginGui extends JFrame {
 
 				if(username.equals("name") && password.equals("password")) {
 					
-					AdminGui s = new AdminGui ();
+					Admin s = new Admin ();
 					s.setVisible(true);
 					close();
 
@@ -118,61 +134,25 @@ public class loginGui extends JFrame {
 
 			
 		});
+		getContentPane().setLayout(null);
+		getContentPane().add(lblLoginDetails);
+		getContentPane().add(lblSantaClaraUniversity);
+		getContentPane().add(lblPassword);
+		getContentPane().add(lblUsername);
+		getContentPane().add(password_field);
+		getContentPane().add(Username_textField);
+		getContentPane().add(btn_login);
+		getContentPane().add(Title);
 		
-		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(22)
-					.addComponent(lblLoginDetails)
-					.addContainerGap(328, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(111, Short.MAX_VALUE)
-					.addComponent(lblSantaClaraUniversity, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE)
-					.addGap(54))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(82)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(lblPassword, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(lblUsername, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-							.addGap(18)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(password_field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(Username_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btn_login, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(134)
-							.addComponent(Title, GroupLayout.PREFERRED_SIZE, 214, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(102, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(16)
-					.addComponent(lblSantaClaraUniversity, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(Title, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblLoginDetails)
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							
-						.addComponent(lblUsername)
-						.addComponent(Username_textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-						.addComponent(password_field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					
-
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btn_login)
-					.addContainerGap(32, Short.MAX_VALUE))
-		);
-		getContentPane().setLayout(groupLayout);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 638, 606);
+		lblNewLabel.setBackground(SystemColor.controlHighlight);
+		lblNewLabel.setIcon(new ImageIcon("/Users/utsavkanpara/Desktop/welcome.jpg"));
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(99, 364, -5, -30);
+		getContentPane().add(lblNewLabel_1);
 	}
 
 
